772f60d1b438499ec0854a007c10e4871439923e
Antonija <antonija.kasum@gmail.com>
2018-10-22 10:16:07 +0300

    Buttons for first/any word

--- src/App.js
+++ src/App.js
@@ -8,22 +8,39 @@ const countries = ['Afghanistan',"Albania","Algeria","Andorra","Angola","Anguill
 class App extends Component {
 
   state = {
-    userInput: ''
+    userInput: '',
+    typeOfSearch: 'first',
+    resultfor: 0
   }
 
   updateInputValue = (e) => {
     this.setState({ userInput: e.target.value});
     this.findMatch()
-  };
+  }
+
+  startingOrAny = (e) => { 
+    if (e.target.value == 'first'){
+    this.setState({ typeOfSearch: 'first'}) 
+  } else {
+    this.setState({ typeOfSearch: 'any'}) 
+  }
+}
 
   findMatch = () => {
     let result = countries.filter((country) => {
       let upperCaseCountry = country.toUpperCase();
-      let userUpper = this.state.userInput.toUpperCase()
-      let match = upperCaseCountry.includes(userUpper);
+      let userUpper = this.state.userInput.toUpperCase();
+      let match;
+      if(this.state.typeOfSearch == 'first'){
+        match = upperCaseCountry.startsWith(userUpper);
+      } else {
+        match = upperCaseCountry.includes(userUpper);
+      }
       if (match){ 
-        return upperCaseCountry}   
+        return upperCaseCountry
+      }   
     });
+
     return result
   }
 
@@ -49,9 +66,9 @@ class App extends Component {
             <span id="start-contain"></span>
             <span id="expression"></span>
           </p>
-          <button type="submit" id="starting-word" className="buttons">Starting Word</button>
-          <button type="submit" id="any-word" className="buttons">Any word</button>
-          <input onChange={this.updateInputValue} type="text" placeholder="Search by..." id="search"/>
+          <button type="submit" id="starting-word" className="buttons" onClick={this.startingOrAny } value="first">Starting Word</button>
+          <button type="submit" id="any-word" className="buttons" onClick={this.startingOrAny } value="any">Any word</button>
+          <input type="text" placeholder="Search by..." id="search" onChange={this.updateInputValue} />
         </header>
         <main>
             {this.findMatch().map((matchCtr, i) =><Country name={matchCtr} id={i} className={this.color(i)} />)}
